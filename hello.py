fa8fd7afcc626323b41816061540dc1e53dddc44
ACHUTHAN MUKUNDAN <achuthanmukundan00@gmail.com>
2024-09-26 11:49:55 -0400

    add bootstrap templates

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -6,8 +6,8 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('user.html', name='Stranger')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
